5a87a5dbc9cc7dd2b77a1ebd17a5d7a624acc1aa
finehang <finehang@gmail.com>
2021-04-16 19:34:07 +0800

    add 今日消耗趋势展示

--- Shiny/app.R
+++ Shiny/app.R
@@ -1,6 +1,13 @@
-pacman::p_load("tidyverse", "shinydashboard", "shiny", "DBI", "lubridate", "pastecs")
+pacman::p_load("tidyverse", "ggrepel", "shinydashboard", "shiny", "DBI", "lubridate", "pastecs")
 setwd("C:/Share")
-connfb_local <- dbConnect(odbc::odbc(), "spend_local", timeout = 10)
+
+get_summary <- function(data){
+  data <- data %>% 
+    stat.desc(basic = T, desc = T, norm = F, p = 0.95) %>%
+    as_tibble_row() %>%
+    transmute(总数 = nbr.val, 总和 = sum, 最大值 = max, 均值 = mean, 方差 = var, 标准差 = std.dev, 中位数 = median, 标准误 = SE.mean, `95%置信区间` = CI.mean.0.95, 变异系数 = coef.var, )
+  return(data)
+}
 
 # Yesterday <- readr::read_csv("./spend_yesterday.csv", col_types = "dccccc") %>%
 #   arrange(-spend)
@@ -10,7 +17,6 @@ connfb_local <- dbConnect(odbc::odbc(), "spend_local", timeout = 10)
 #   arrange(-spend)
 # Today <- readr::read_csv("./spend_today.csv", col_types = "dccccc") %>%
 #   arrange(-spend)
-
 
 ui <- fluidPage(
   titlePanel("Welcome to GatherOne"),
@@ -37,39 +43,29 @@ ui <- fluidPage(
 )
 
 server <- function(input, output) {
+  
+  connfb_local <- dbConnect(odbc::odbc(), "spend_local", timeout = 10)
+  
   Quarter <- dbGetQuery(connfb_local, "select * from spend_quarter order by spend DESC")
 
   Month <- dbGetQuery(connfb_local, "select * from spend_month order by spend DESC")
 
   Yesterday <- dbGetQuery(connfb_local, "select * from spend_yesterday order by spend DESC")
 
-  Today <- dbGetQuery(connfb_local, "select * from spend.spend_today where update_time = (select distinct(update_time) as date_time from spend.spend_today order by date_time desc limit 1)")
+  Today <- dbGetQuery(connfb_local, "select * from spend.spend_today where update_time = (select distinct(update_time) as date_time from spend.spend_today order by date_time desc limit 1) order by spend DESC")
 
-  History <- dbGetQuery(connfb_local, "select max(`sum`) as `sum`, `hour` from (SELECT sum(spend) as `sum`, hour(update_time) as `hour`  FROM spend.spend_today where DATE(update_time) = CURDATE() group by update_time) as `all` group by hour")
-
-
-  Quarter_s <- Quarter$spend %>%
-    stat.desc(basic = T, desc = T, norm = F, p = 0.95) %>%
-    as_tibble_row() %>%
-    transmute(总数 = nbr.val, 总和 = sum, 最大值 = max, 均值 = mean, 方差 = var, 标准差 = std.dev, 中位数 = median, 标准误 = SE.mean, `95%置信区间` = CI.mean.0.95, 变异系数 = coef.var, )
-  Month_s <- Month$spend %>%
-    stat.desc(basic = T, desc = T, norm = F, p = 0.95) %>%
-    as_tibble_row() %>%
-    transmute(总数 = nbr.val, 总和 = sum, 最大值 = max, 均值 = mean, 方差 = var, 标准差 = std.dev, 中位数 = median, 标准误 = SE.mean, `95%置信区间` = CI.mean.0.95, 变异系数 = coef.var, )
-  Yesterday_s <- Yesterday$spend %>%
-    stat.desc(basic = T, desc = T, norm = F, p = 0.95) %>%
-    as_tibble_row() %>%
-    transmute(总数 = nbr.val, 总和 = sum, 最大值 = max, 均值 = mean, 方差 = var, 标准差 = std.dev, 中位数 = median, 标准误 = SE.mean, `95%置信区间` = CI.mean.0.95, 变异系数 = coef.var, )
-  Today_s <- Today$spend %>%
-    stat.desc(basic = T, desc = T, norm = F, p = 0.95) %>%
-    as_tibble_row() %>%
-    transmute(总数 = nbr.val, 总和 = sum, 最大值 = max, 均值 = mean, 方差 = var, 标准差 = std.dev, 中位数 = median, 标准误 = SE.mean, `95%置信区间` = CI.mean.0.95, 变异系数 = coef.var, )
+  History <- dbGetQuery(connfb_local, "SELECT sum(spend) AS `sum`, update_time FROM spend.spend_today WHERE DATE(update_time) = CURDATE() GROUP BY update_time") %>% 
+    mutate(sum = round(sum, 2), 
+           time = hour(update_time) + minute(update_time) / 60)
+  
+  data_all <- list(Quarter = Quarter$spend, Month = Month$spend, Yesterday = Yesterday$spend, Today = Today$spend) %>% 
+    map_df(~get_summary(.))
 
   output$t0 <- renderTable(switch(input$date,
-    "Quarter" = Quarter_s,
-    "Month" = Month_s,
-    "Yesterday" = Yesterday_s,
-    "Today" = Today_s
+    "Quarter" = data_all[1,],
+    "Month" = data_all[2,],
+    "Yesterday" = data_all[3,],
+    "Today" = data_all[4,]
   ))
   output$t1 <- renderTable(switch(input$date,
     "Quarter" = Quarter,
@@ -80,8 +76,16 @@ server <- function(input, output) {
   
   output$p1 <- renderPlot(
     History %>% 
-      ggplot(mapping = aes(x = hour, y = sum)) +
-      geom_line()
+      ggplot(mapping = aes(x = time, y = sum)) +
+      geom_line() +
+      geom_point() + 
+      xlab("Time") +
+      ylab("Spend") +
+      geom_smooth() +
+      scale_y_continuous(limits = c(0, max(History$sum)), breaks = seq(0, max(History$sum), 30000)) +
+      scale_x_continuous(limits = c(0, 24), breaks = seq(0, 24, 2)) +
+      geom_label_repel(aes(label = sum)) +
+      ggtitle("今日消耗增长趋势")
   )
 }
 
